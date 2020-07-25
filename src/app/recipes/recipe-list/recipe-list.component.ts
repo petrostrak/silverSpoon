0282521821824cb98e43ab4da07ab4623faaeee8
petros <pit.trak@gmail.com>
2020-07-25 11:35:45 +0300

    added several recipes and rendered them with ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'Desc', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg')
+    new Recipe('Pasta recipe', 'With tomato paste', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/spaghetti-puttanesca_1.jpg'),
+    new Recipe('Punjabi samosa recipe', 'Crisp fried Indian snack', 'https://www.indianhealthyrecipes.com/wp-content/uploads/2019/11/samosa-recipe-480x270.jpg'),
+    new Recipe('Bratwurst recipe', 'In beer with onions', 'https://www.foodiecrush.com/wp-content/uploads/2012/10/Bratwurst-foodiecrush.com-013-500x500.jpg')
   ];
 
   constructor() { }
