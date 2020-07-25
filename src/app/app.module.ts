8591897024d2c0f308534ab1953791f2e276891d
petros <pit.trak@gmail.com>
2020-07-25 19:20:27 +0300

    using services for pushing data from a to b

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { BrowserModule } from '@angular/platform-browser';
@@ -26,7 +27,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ShoppingListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
