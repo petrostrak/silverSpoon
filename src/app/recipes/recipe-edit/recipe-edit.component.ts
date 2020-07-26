dcea7b6690e6aff098852b9699e1ed1e25278129
petros <pit.trak@gmail.com>
2020-07-26 16:26:25 +0300

    adding new ingredient controls

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -66,4 +66,13 @@ export class RecipeEditComponent implements OnInit {
   getControls() {
     return (<FormArray>this.recipeForm.get('ingredients')).controls;
   }
+
+  onAddIngredient(){
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name': new FormControl(),
+        'amount': new FormControl()
+      })
+    )
+  }
 }
