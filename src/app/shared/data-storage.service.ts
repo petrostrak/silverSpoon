20fca3ce8b039ba5ce2a692d1342f109b9b5f195
petros <pit.trak@gmail.com>
2020-07-26 20:42:25 +0300

    Stored data to firebase

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,8 +1,16 @@
+import { RecipeService } from './../recipes/recipe.service';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 
 @Injectable({ providedIn: 'root'})
 export class DataStorageService {
-    constructor(private http: HttpClient){}
+    constructor(private http: HttpClient, private recipeService: RecipeService){}
+
+    storeRecipes(){
+        const recipes = this.recipeService.getRecipes();
+        this.http.put('https://silver-spoon-2e1cf.firebaseio.com/recipes.json', recipes).subscribe(response => {
+            console.log(response)
+        })
+    }
 }
