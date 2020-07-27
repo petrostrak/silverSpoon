ef17c2dde26ca6233b4489e65d3477f968abe2e6
petros <pit.trak@gmail.com>
2020-07-27 11:22:58 +0300

    fixed resolver issue where edited data wouldn't persist

--- src/app/recipes/recipes.resolver.service.ts
+++ src/app/recipes/recipes.resolver.service.ts
@@ -1,3 +1,4 @@
+import { RecipeService } from './recipe.service';
 import { DataStorageService } from './../shared/data-storage.service';
 import { Recipe } from './recipe.model';
 import { Injectable } from '@angular/core';
@@ -7,9 +8,15 @@ import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/r
 @Injectable({ providedIn: 'root' })
 export class RecipesResolverService implements Resolve<Recipe[]>{
     
-    constructor(private dataStorageService: DataStorageService){}
+    constructor(private dataStorageService: DataStorageService, private recipeService: RecipeService){}
 
     resolve(route:ActivatedRouteSnapshot, state: RouterStateSnapshot){
-        return this.dataStorageService.fetchRecipes();
+        const recipes = this.recipeService.getRecipes();
+       
+        if(recipes.length === 0){
+            return this.dataStorageService.fetchRecipes();
+        }else{
+            return recipes;
+        } 
     }
 }
