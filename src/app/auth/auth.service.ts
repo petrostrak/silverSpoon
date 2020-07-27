17a2f744e9793401d4adc5fc56f19c979bf12fdc
petros <pit.trak@gmail.com>
2020-07-27 20:02:54 +0300

    Added localStorage for persistence

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -54,10 +54,30 @@ export class AuthService {
         this.router.navigate(['/auth'])
     }
 
+    autoLogin(){
+        const userData: {
+            email: string;
+            id: string;
+            _token: string;
+            _tokenExpirationData: string;
+        } = JSON.parse(localStorage.getItem('userData'))
+
+        if(!userData){
+            return;
+        }
+
+        const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationData))
+        
+        if(loadedUser.token){
+            this.user.next(loadedUser)
+        }
+    }
+
     private handleAuthentication(email: string, userId: string, token: string, expiresIn: number){
         const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
         const user = new User(email, userId, token, expirationDate);
         this.user.next(user);
+        localStorage.setItem('userData', JSON.stringify(user))
     }
 
     private handleError(errorResponse: HttpErrorResponse) {
