dd662a00a329fae36d46d92ff0d1135056af69bc
petros <pit.trak@gmail.com>
2020-07-28 15:49:49 +0300

    Implemented Lazy Loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,9 @@ import { AuthModule } from './auth/aut.module';
 import { CoreModule } from './core.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
-import { RecipesModule } from './recipes/recipes.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -21,7 +20,6 @@ import { HttpClientModule } from '@angular/common/http';
     ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
-    RecipesModule,
     ShoppingListModule,
     SharedModule,
     CoreModule,
